bd0bab538295742f770f63c82490ff5e75018688
Ivan <think8about23it@gmail.com>
2018-04-21 21:47:52 +0300

    Simple User Authentification
    First Web App

--- justaproject/src/main/java/app/exceptions/EvenAmountException.java
+++
@@ -1,7 +0,0 @@
-package app.exceptions;
-
-public class EvenAmountException extends Exception {
-    public EvenAmountException(String s) {
-        super(s);
-    }
-}
--- justaproject/src/main/java/app/exceptions/NotEnoughVariantsException.java
+++
@@ -1,7 +0,0 @@
-package app.exceptions;
-
-public class NotEnoughVariantsException extends Exception {
-    public NotEnoughVariantsException(String s) {
-        super(s);
-    }
-}
